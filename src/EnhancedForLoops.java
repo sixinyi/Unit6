1c2c55a6c8cfd7dacbc154696248f898289b13a3
sixinyi <yangwenxuans@outlook.com>
2019-12-18 13:51:42 -0500

    12/18 Bruce update everything

---
+++ src/EnhancedForLoops.java
@@ -0,0 +1,87 @@
+// Bruce 12/18 3 same program for test program
+public class EnhancedForLoops {
+    public static String printStandardFor(String[] arr){
+
+        String RS = "";
+
+        for(int n = 0; n < arr.length; n++)
+            RS += arr[n] + " ";
+        return RS;
+    }
+    public static String printStandardFor(int[] arr){
+
+        String RS = "";
+
+        for(int n = 0; n < arr.length; n++)
+            RS += arr[n] + " ";
+        return RS;
+    }
+    public static String printStandardFor(double[] arr){
+
+        String RS = "";
+
+        for(int n = 0; n < arr.length; n++)
+            RS += arr[n] + " ";
+        return RS;
+    }
+    public static String printEnhancedFor(String[] arr){
+
+        String RS = "";
+
+        for(String x:arr){
+            RS += x + " ";
+        }
+        return RS;
+    }
+    public static String printEnhancedFor(int[] arr){
+
+        String RS = "";
+
+        for(int x:arr){
+            RS += x + " ";
+        }
+        return RS;
+    }
+    public static String printEnhancedFor(double[] arr){
+
+        String RS = "";
+
+        for(double x:arr){
+            RS += x + " ";
+        }
+        return RS;
+    }
+    public static String printStandardWhile(String[] arr){
+
+        int count = 0;
+        String RS = "";
+
+        while(count < arr.length) {
+            RS += arr[count] + " ";
+            count++;
+        }
+        return RS;
+    }
+    public static String printStandardWhile(int[] arr){
+
+        int count = 0;
+        String RS = "";
+
+        while(count < arr.length) {
+            RS += arr[count] + " ";
+            count++;
+        }
+        return RS;
+    }
+    public static String printStandardWhile(double[] arr){
+
+        int count = 0;
+        String RS = "";
+
+        while(count < arr.length) {
+            RS += arr[count] + " ";
+            count++;
+        }
+        return RS;
+    }
+}
